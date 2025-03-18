1e1aaaa9110905f6e14d2182b8744cd45f489c3a
Shailja-9044 <shail.work12345@gmail.com>
2025-03-18 03:24:57 +0000

    firstcommit

---
+++ secondproject/secondproject/views.py
@@ -0,0 +1,21 @@
+from django.shortcuts import HttpResponse
+
+data=f"<hr><a href='/'>Home</a>  <a href='/signup'>SignUp</a>  <a href='/signin'>SignIn</a>"
+def index(req):
+    data=f"<hr><a href=#>Home</a>  <a href='signup'>SignUp</a>  <a href='signin'>SignIn</a>"
+    return HttpResponse(f"<center><h1>Welcome to my Page!{data}</h1></center>")
+
+def signup(req):
+    global username
+    username=input("enter username=")
+    return HttpResponse(f"<center><h1>Signup Page{data}</h1></center>")
+
+def signin(req):
+    chkusername=input("enter username to signin=")
+    if chkusername==username:
+        next=f"<hr><h1><a href='/'>Logout</a>"
+        return HttpResponse(f"<center><h1>Welcome {chkusername}!!!{next}</h1></center>")
+    else:
+        msg=f"<center><h1>Incorrect Username!! Try again</h1></center>"
+        next=f"<hr><h1><a href='/'>Click here to go Back</a>"
+        return HttpResponse(f"{msg}{next}")
